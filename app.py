29d24289c4e52223cf712afc2bd005be7e65adf0
katehuntsman <s577328@nwmissouri.edu>
2024-10-28 13:31:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -110,3 +110,17 @@ with ui.layout_columns():
                 ax.set_xlabel(input.selected_attribute())
                 ax.set_ylabel("Number")
                 return ax
+
+# --------------------------------------------------------
+# Reactive calculations and effects
+# --------------------------------------------------------
+
+# Add a reactive calculation to filter the data
+# By decorating the function with @reactive, we can use the function to filter the data
+# The function will be called whenever an input functions used to generate that output changes.
+# Any output that depends on the reactive function (e.g., filtered_data()) will be updated when the data changes.
+
+@reactive.calc
+def filtered_data():
+    return penguins_df
+
